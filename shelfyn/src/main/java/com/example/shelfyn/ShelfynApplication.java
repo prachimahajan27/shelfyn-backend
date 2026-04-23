03894f8664b0765f7eadc418b20844d054a119dc
prachimahajan27 <prachimahajan631@gmail.com>
2026-04-24 00:25:53 +0530

    First commit

---
+++ shelfyn/src/main/java/com/example/shelfyn/ShelfynApplication.java
@@ -0,0 +1,13 @@
+package com.example.shelfyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfynApplication.class, args);
+	}
+
+}
